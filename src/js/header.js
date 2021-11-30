f2cabb0635d6a458efba9e43f2951a426b72cda7
Literal Line <13988479+literal-line@users.noreply.github.com>
2021-11-30 10:53:52 -0500

    rewrite links

--- src/js/header.js
+++ src/js/header.js
@@ -12,8 +12,8 @@
     '</a>' +
     '<nav>' +
     '<a ' + (currentPage === '/' || currentPage === '/index' ? 'data-page-current' : '') + ' href="/">Home</a>' +
-    '<a ' + (currentPage === '/blog' ? 'data-page-current' : '') + ' href="/blog.html">Blog</a>' +
-    '<a ' + (currentPage === '/gallery' ? 'data-page-current' : '') + ' href="/gallery.html">Gallery</a>' +
+    '<a ' + (currentPage === '/blog' ? 'data-page-current' : '') + ' href="/blog">Blog</a>' +
+    '<a ' + (currentPage === '/gallery' ? 'data-page-current' : '') + ' href="/gallery">Gallery</a>' +
     '</nav>';
   window.addEventListener('load', function () {
     d.body.insertAdjacentElement('afterbegin', header);
