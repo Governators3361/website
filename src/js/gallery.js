e0fa30eb50670b0f75c9f3b772c57050c88a153f
Lɪᴛᴇʀᴀʟ Lɪɴᴇ <chickenfishbird2@gmail.com>
2022-02-01 18:13:21 -0500

    gallery works now maybe

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -3,9 +3,27 @@
 (function (d) {
   'use strict';
 
-  window.addEventListener('load', function () {
-    var container = d.getElementById('gallery');
-    container.classList.add('panel');
-    console.log(container)
-  });
+  var listing;
+  var r = new XMLHttpRequest();
+
+  var load = function () {
+    listing = r.responseText.replace(/\r/gm, '').split('\n').map(function (cur) { return { name: cur.split('/').pop().split('.')[0], src: cur }; });
+
+    var containers = Array.prototype.slice.call(d.getElementsByClassName('gallery'));
+    containers.forEach(setupContainer);
+  };
+
+  var setupContainer = function (container) {
+    listing.forEach(function (curImage) {
+      var card = document.createElement('div');
+      card.classList.add('panel');
+      card.innerHTML = `<img src="${curImage.src}"></img><div>${curImage.name}</div>`;
+      container.appendChild(card);
+    });
+  };
+
+  r.open('GET', '/gallery/listing.txt');
+  r.onload = load;
+
+  window.addEventListener('load', function () { r.send(); });
 })(document);
